df3280a27b75f6d5423f3b0ffd12bb3ece199e2f
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2021-01-24 18:15:26 +0100

    created cart menu list

--- src/components/TheCartList.vue
+++ src/components/TheCartList.vue
@@ -1,10 +1,34 @@
 <template>
-  <div></div>
+  <article class="cart-list">
+    <div
+      class="cart-list__item"
+      v-for="cartItem in cartItems"
+      :key="cartItem.id"
+    >
+      <div class="cart-list__row">
+        <div class="cart-list__col">
+          <p class="cart-list__name"></p>
+          <p class="cart-list__price"></p>
+        </div>
+      </div>
+      <div class="cart-list__row">
+        <div class="cart-list__col"></div>
+      </div>
+    </div>
+  </article>
 </template>
 
 <script>
+import store from "@/store";
+import { computed } from "vue";
 export default {
   name: "TheCartList",
+  setup() {
+    const cartItems = computed(() => store.state.cartItems);
+    return {
+      cartItems,
+    };
+  },
 };
 </script>
 
